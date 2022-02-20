b638bca2279666ad49e3c67b2ab55476cd9a65ea
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-19 23:34:37 -0500

    seed from Api plus pimped up index page

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true, length: { maximum: 30 }
 end
